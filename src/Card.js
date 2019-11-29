0ac94502f5f9bfc1e546b81ed6d3e9a702232b6f
Heather Faerber <heatherdfaerber@gmail.com>
2019-11-29 16:45:41 -0700

    Rebuild card and ideas and add delete functionality

--- src/Card.js
+++ src/Card.js
@@ -1,1 +1,15 @@
 import React from 'react'
+import './Card.css'
+
+const Card = (props) => {
+  const {title, id, description, deleteIdea} = props;
+  return(
+    <div className="card">
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <button onClick={event => deleteIdea(id)}>🗑</button>
+    </div>
+  )
+}
+
+export default Card
--- src/Ideas.js
+++ src/Ideas.js
@@ -1,1 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './Ideas.css';
+import Card from './Card'
+
+const Ideas = ({ideas, deleteIdea}) => {
+  const ideaCards = ideas.map(idea => {
+    return <Card
+      key={idea.id}
+      id={idea.id}
+      title={idea.title}
+      description={idea.description}
+      deleteIdea={deleteIdea}
+    />
+  })
+
+  return(
+    <div className="ideas-container">
+      {ideaCards}
+    </div>
+  )
+}
+
+export default Ideas
